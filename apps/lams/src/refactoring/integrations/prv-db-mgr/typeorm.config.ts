04e1658a87f8b1e752ee74a1229ca2c9c90c0a26
Kyuhyun Kim <ggh163700@gmail.com>
2026-01-26 17:21:15 +0900

    fix: update database connection settings - changed PostgreSQL host, username, password, and database name for integration with Supabase.

--- apps/lams/src/refactoring/integrations/prv-db-mgr/typeorm.config.ts
+++ apps/lams/src/refactoring/integrations/prv-db-mgr/typeorm.config.ts
@@ -3,11 +3,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const getTypeOrmConfig = (): TypeOrmModuleOptions => {
     const config: TypeOrmModuleOptions = {
         type: 'postgres',
-        host: '192.168.10.11',
+        host: 'aws-1-ap-northeast-2.pooler.supabase.com',
         port: 5432,
-        username: 'admin',
-        password: 'tech7admin!',
-        database: 'attendance-server',
+        username: 'postgres.txpqhatvkesbhjrsnhgj',
+        password: 'xSDsnZxHH2NJgVse',
+        database: 'postgres',
         autoLoadEntities: true,
     };
     return config;
